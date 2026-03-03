0110982b817d8641e1d414adecd799a3af74acdb
Jakob <jakobfred1999@gmail.com>
2026-03-03 18:03:32 +1300

    Add descriptions of each expansion to DisplayStartGameMessage()

--- Program.cs
+++ Program.cs
@@ -78,12 +78,14 @@ public class FountainGame
     {
         ConsoleColor foregroundColor = Console.ForegroundColor;
         Console.ForegroundColor = ConsoleColor.Magenta;
-        Console.WriteLine("You enter the Cavern of Objects, a maze of rooms filled with dangerous pits in search of the Fountain of Objects.");
-        Console.WriteLine("Light is visible only in the entrance, and no other light is seen anywhere in the caverns.");
-        Console.WriteLine("You must navigate the Caverns with your other senses.");
-        Console.WriteLine("Find the Fountain of Objects, enable it, and return to the entrance.");
-        Console.WriteLine("Look out for pits. You will feel a breeze if a pit is in an adjacent room. If you enter a room with a pit, you will die.");
-        Console.ForegroundColor = foregroundColor;
+        Console.WriteLine("- You enter the Cavern of Objects, a maze of rooms filled with dangerous pits in search of the Fountain of Objects.");
+        Console.WriteLine("- Light is visible only in the entrance, and no other light is seen anywhere in the caverns.");
+        Console.WriteLine("- You must navigate the Caverns with your other senses.");
+        Console.WriteLine("- Find the Fountain of Objects, enable it, and return to the entrance.");
+        Console.WriteLine("- Look out for pits. You will feel a breeze if a pit is in an adjacent room. If you enter a room with a pit, you will die.");
+        Console.WriteLine("- Maelstroms are violent forces of sentient wind. Entering a room with one could transport you to any other location in the caverns. You will be able to hear their growling and groaning in nearby rooms");
+        Console.WriteLine("- Amaroks roam the caverns. Encountering one is certain death, but you can smell their rotten stench in nearby rooms.");
+        Console.WriteLine("- You carry with you a bow and a quiver of arrows. You can use them to shoot monsters in the caverns but be warned: you have a limited supply.");        Console.ForegroundColor = foregroundColor;
 
         Console.WriteLine();
         Console.Write("Press any key to continue...");
